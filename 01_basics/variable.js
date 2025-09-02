68aa88154f81ae2b8c6e8ca50fe0a20d12256ff3
dhaneeshgupta <officialhariomgupra16@gmail.com>
2025-09-02 18:19:59 +0530

    first commit

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+ const accountID = 1442344
+ let accountEmail = "tilli01@gmail.com"
+ var accountPassword = 12324
+accountCity = "Indore"
+let accountState
+
+
+//  accountID = 3  // not allowed
+
+accountEmail = "hlo@gmail.comcleas"
+accountPassword = 12133
+ accountCity= "Bhopal"
+
+// Prefer not to use var because issue in block scope and function scope :
+ console.log(accountID);
+ console.table([accountID, accountEmail, accountPassword,accountCity, accountState ])
+ 
---
+++ 02_basics/dataTypes.js
@@ -0,0 +1,14 @@
+let name = "Tilllu"
+let age = 42
+let IsLogin = true
+
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => Stand alone value / intetionally defined for variable
+// undefined =>
+// symbol => Unique 
+
+console.log(typeof null) // Data Type of Null is object
+console.log(typeof undefined) // undefined is remain same 
