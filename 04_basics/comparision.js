5225cad3f8309f7a1430380fe66c9b7fc1464aef
dhaneeshgupta <officialhariomgupra16@gmail.com>
2025-09-02 23:49:12 +0530

    Comparision updated

---
+++ 04_basics/comparision.js
@@ -0,0 +1,17 @@
+/*console.log("2" > 1)
+console.log("02" > 1)  
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined == 0)
+console.log(undefined >  0)
+console.log(undefined <  0) */ 
+
+
+// === is check equality but also check data type 
+// == it directly compare 
+
+console.log("2" === 2); // give false because not same data type
