321e478a63ab1fafeef55422b54577a04db994ed
dhaneeshgupta <officialhariomgupra16@gmail.com>
2025-09-02 23:22:55 +0530

    conversion file updated

---
+++ 03_basics/conversionOperation.js
@@ -0,0 +1,33 @@
+let score = null
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let ValueInNumber = Number(score) // Conversion Of string into Number
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+
+// if in string "33" => 33 after conversion 
+// if in string "33abc" and having some var the => NAN "Not a number"
+// If NULL => 0
+// if Boolean => 1/0 
+
+let IsLoggedIn = "Harry"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// If 1 => true
+// if 0 => false
+// if any name/string => True
+// if "" => false
+
+
+let SomeNumber = 13
+
+let stringIsLoggedIn = String(SomeNumber)
+console.log(stringIsLoggedIn)
+
+console.log(typeof(stringIsLoggedIn))
