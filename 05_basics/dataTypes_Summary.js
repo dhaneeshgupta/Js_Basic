ca3cb43ecad0fa0134ea16685bd50164a456bcc9
dhaneeshgupta <officialhariomgupra16@gmail.com>
2025-09-03 12:44:29 +0530

    Data Types Updated

---
+++ 05_basics/dataTypes_Summary.js
@@ -0,0 +1,33 @@
+// Primitive Data types:
+// TYPES :String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const ScoreValue = 100.3
+
+const IsLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const Id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(Id == anotherId) // return false
+
+
+const BigNumber =2323434534634734
+
+// Reference (Non - Primitive)
+
+// Array, Object, Function
+
+const heros = ["Batman", "Iron Man", 'Hulk', 'Thor'];
+ const myObj = {
+    name : 'Harry',
+    age  : 20,
+}
+
+
+// Function : Variable Type
+// Functon Object :
+const myFunction = function(){
+    console.log("Hello Harry")
+}
